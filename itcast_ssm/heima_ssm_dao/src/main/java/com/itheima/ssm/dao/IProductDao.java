74b67d93f0a73d282fa2858baa4785274f6c2a7f
chen <1018607@sdi.com>
2019-11-24 18:05:10 +0800

    第一次提交

---
+++ itcast_ssm/heima_ssm_dao/src/main/java/com/itheima/ssm/dao/IProductDao.java
@@ -0,0 +1,13 @@
+package com.itheima.ssm.dao;
+
+import cao.itheima.ssm.domain.Product;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface IProductDao {
+    @Select("select * from product")
+    public List<Product> findAll() throws Exception;
+
+    ;
+}
